b732a8f1ba2b45f5cf7fa6341a7decec2361230c
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-04-04 10:15:49 -0400

    Add random greeting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "root#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
+
+  root "static#index"
 end
